c919e1b02d874cc91aa357703029bd2a3a40f51f
SUJATHA KANNAN <ksujatha625@gmail.com>
2024-12-20 12:28:56 +0100

    Base Code

---
+++ src/main/kotlin/com/finadem/FinaDemApp.java
@@ -0,0 +1,11 @@
+package com.finadem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinaDemApp {
+    public static void main(String[] args) {
+        SpringApplication.run(FinaDemApp.class, args);
+    }
+}
---
+++ src/test/kotlin/com/finadem/FinaDemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.finadem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FinaDemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
